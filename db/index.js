675698be12856076c75748a7f463d9304db21545
MikeJDev <ukmjanes@gmail.com>
2019-03-19 12:21:27 -0500

    feat(create database query variables in the db/index.js file and export those queries to the server)
    create database query variables in the db/index.js file and export those queries to the server
    
    #65

--- db/index.js
+++ db/index.js
@@ -1,9 +1,52 @@
 const config = require('../knexfile')['production'];
 const knex = require('knex')(config)
-const sql = knex('test table').then((result) => {
-  console.log('successful open connection to hosted database')
+
+const initialConnection = knex('test table').then((result) => {
+  console.log('successful connection to hosted database, lets go team!')
 }).catch((err) => {
   console.log('err:', err)
 })
 
-module.exports = sql
+const findAllCandidates = (err, cb) => {
+  knex('candidates')
+    .then((results) => {
+      cb(null, results)
+      console.log('results:', results)
+    })
+    .catch((err) => {
+      cb(err)
+      console.log('err:', err)
+    })
+}
+
+const findAllPolicies = (err, cb) => {
+  knex('policies')
+    .then((results) => {
+      cb(null, results)
+      console.log('results:', results)
+    })
+    .catch((err) => {
+      cb(err)
+      console.log('err:', err)
+    })
+}
+
+const findAllVoterId = (err, cb) => {
+  knex('voter-id')
+    .then((results) => {
+      cb(null, results)
+      console.log('results:', results)
+    })
+    .catch((err) => {
+      cb(err)
+      console.log('err:', err)
+    })
+}
+
+module.exports = {
+  initialConnection,
+  findAllCandidates,
+  findAllPolicies,
+  findAllVoterId
+
+}
