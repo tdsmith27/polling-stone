9cc926c7c5ef147f9a1a4bd814e3e2e45f7f35bd
MikeJDev <ukmjanes@gmail.com>
2019-03-19 14:14:10 -0500

    feat(server/routes/candidates.js): Create route from server to database to retrieve all candidates i
    Create route from server to database to retrieve all candidates info
    
    #65

--- server/routes/candidates.js
+++ server/routes/candidates.js
@@ -1,10 +1,17 @@
 const router = require("express").Router();
+const db = require('../../db/index.js')
 
-// Test GET
 router.get("/", (req, res) => {
-  res.json({ data: ["dolphins", "manatees", "sea turles"] });
-});
-
+  db.findAllCandidates(null, (err, results) => {
+    if (err) {
+      console.log(err)
+      res.end()
+    } else {
+      console.log("success candidates retrieval")
+      res.status(200).json(results)
+    }
+  })
+})
 // Routes for Database
 
 // Routes for APIs start here
