48e4ae1a61b0eaac8a61b494d299e0323eed293b
Remington Davis <remy@Remingtons-MacBook-Pro.local>
2019-03-19 10:47:14 -0500

    Unimportant Changes

--- client/src/__tests__/Map.test.js
+++ client/src/__tests__/Map.test.js
@@ -2,14 +2,13 @@ import React from 'react';
 import {render, fireEvent, cleanup, waitForElement} from 'react-testing-library';
 
 import 'jest-dom/extend-expect';
-import PollMap from '../components/PollMap.js';
+import PollMa from '../components/PollMap.js';
 
 afterEach(cleanup);
 
 it('Should render a map component', async () => {
-    const map = render(<PollMap />,);
-    console.log('we in diss bitch')
+    const map = render(<PollMa />,);
     // const map2 = await waitForElement(() => getByText(/map/i),)
 
-    // expect(map).toBeTruthy();
+    expect(map).toBeTruthy();
 });
--- client/src/components/PollMap.js
+++ client/src/components/PollMap.js
@@ -1,23 +1,34 @@
 import React, { Component } from 'react';
-import ReactMapGL from 'react-map-gl';
+import mapboxgl from 'mapbox-gl';
+import ReactMapboxGl, {Marker} from 'react-mapbox-gl';
+
+
+const Map = ReactMapboxGl({
+  accessToken: "pk.eyJ1IjoiZGlub3N0YXJwbHVzIiwiYSI6ImNqdGVrZGFmbjAxZ3I0OW1ndGU0M2w1NHEifQ.NXU-IzfFbmcZ1v99GAqQSA",
+});
+
 class PollMap extends Component {
-    state = {
-        viewport: {
-          width: 400,
-          height: 400,
-          latitude: 37.7577,
-          longitude: -122.4376,
-          zoom: 8
+      constructor(props) {
+        super(props);
+        this.state = {
+          markers: [[-0.2416815, 51.5285582], [0.2416816, 31.5285583], [0.2416816, 31.5285583], [10.2416816, 31.5285583], [40.2416816, 31.5285583], [20.2416816, 31.5285583], [0.3416816, 21.5285583], [10.2416816, 1.5285583]]
         }
-      };
+      }
       render() {
         return (
-          <ReactMapGL
-            {...this.state.viewport}
-            onViewportChange={(viewport) => this.setState({viewport})}
-            mapboxApiAccessToken={'pk.eyJ1IjoiZGV2bm9haCIsImEiOiJjanRieGNpZzAwcW5tNDRyeXhvbG5tZjZjIn0.tB7Rsz3NKcNnpwfWWMltVg'}
-          />
-        );
+          
+          <Map style="mapbox://styles/mapbox/streets-v8" containerStyle={{width: '500px', height: '500px'}} >
+          {this.state.markers.map((pin) => {
+            return (
+              <Marker
+                coordinates={pin}
+                anchor="bottom">
+                <div>X</div>
+              </Marker>
+            )
+          })}
+          </Map>
+        )
     }
 }
 
