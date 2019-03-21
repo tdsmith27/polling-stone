409a023e7d84c667b1a01daa29dd1621de414690
Trevor Smith <smittrevor@gmail.com>
2019-03-21 11:50:17 -0500

    build(Production build): remove build folder from .gitignore for production

---
+++ client/build/precache-manifest.7265d10ce7af712f8b66e4ec8bde2c15.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "b9a2dc637885fe953b43",
+    "url": "/static/css/main.36237492.chunk.css"
+  },
+  {
+    "revision": "b9a2dc637885fe953b43",
+    "url": "/static/js/main.a51e900d.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "be2a1378d5fef3edf34e",
+    "url": "/static/css/2.bfe73d85.chunk.css"
+  },
+  {
+    "revision": "be2a1378d5fef3edf34e",
+    "url": "/static/js/2.878b678d.chunk.js"
+  },
+  {
+    "revision": "6c262146787e7624aa1e2652a9ae38ae",
+    "url": "/static/media/polls.6c262146.jpg"
+  },
+  {
+    "revision": "45586c9673a1efa1c71121f952c78629",
+    "url": "/static/media/voting.45586c96.jpg"
+  },
+  {
+    "revision": "481e89e72867165276ca7c1cef38abf7",
+    "url": "/index.html"
+  }
+];
