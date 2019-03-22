6197d0fe8240a111311da37761996e84fdf8f1b9
Dan Kaiser <Tophatman100@gmail.com>
2019-03-22 12:11:38 -0500

    chore(PolicyBlock.js): Changed title from Adoption to Abortion in policy card (#170)
    #167

--- client/src/components/CandidateDetailComponents/PolicyBlock.js
+++ client/src/components/CandidateDetailComponents/PolicyBlock.js
@@ -2,17 +2,17 @@ import React from 'react';
 import PolicyCard from './PolicyCard.js';
 
 const PolicyBlock = (props) => {
-  const { abortion, lgbtAdoption, gunControl, equalPay, climateChange, gayMarriage, borderWall, plannedParrenthood } = props.policies; //sic
+  const { abortion, lgbtAdoption, gunControl, equalPay, climateChange, gayMarriage, borderWall, plannedParrenthood } = props.policies;
   const policies = [abortion, lgbtAdoption, gunControl, equalPay, climateChange, gayMarriage, borderWall, plannedParrenthood];
-  const policyNames = ['Adoption', 'LGBT Adoption', 'Gun Control', 'Equal Pay', 'Climate Change', 'Gay Marriage', 'Border Wall', 'Planned Parenthood'];
-  
+  const policyNames = ['Abortion', 'LGBT Adoption', 'Gun Control', 'Equal Pay', 'Climate Change', 'Gay Marriage', 'Border Wall', 'Planned Parenthood'];
+
   const divStyle = {
     display: 'inline-flex',
     flexWrap: 'wrap'
   }
-  return( 
+  return (
     <div style={divStyle}>
-      {policies.map((el, ind) => <PolicyCard title={policyNames[ind]} policy={el}/>)}
+      {policies.map((el, ind) => <PolicyCard title={policyNames[ind]} policy={el} />)}
     </div>
   )
 };
